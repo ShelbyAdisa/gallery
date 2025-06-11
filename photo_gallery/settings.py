71da322e2bddfd4924bcc028490846ef334a5032
ShelbyAdisa <sheladisaa@gmail.com>
2025-06-11 19:39:54 +0300

    gallery

--- photo_gallery/settings.py
+++ photo_gallery/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'SECRET_KEY'
+SECRET_KEY = 'django-insecure-u2-t=3yh2b8j694#h!my&039m1+zp)l-tgs8uk%hc6gk$_r8am'
 #  'django-insecure-u2-t=3yh2b8j694#h!my&039m1+zp)l-tgs8uk%hc6gk$_r8am'
 
 # SECURITY WARNING: don't run with debug turned on in production!p
